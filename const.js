7ffec7d92803f880b79e2401133471e02211f12d
Vladkvarta <Vladkvarta@gmail.com>
2022-06-24 23:57:21 +0300

    master 1.003

--- const.js
+++ const.js
@@ -1,5 +1,15 @@
-const commands = `
+const command = `
 /start - перезапустить бота
 /help - помощь
+/positions - список товаров
 `
-module.exports.commands = commands
+const nut= `
+    Горішок зі згущеним молоком
+    Вага: 20 грммів
+    Термін придатності: 7 днів
+    Склад: Згущене молоко, фундук дроблений, пісочне печиво
+    Опис: "тут должно быть описание"
+`
+
+module.exports.commands = command
+module.exports.nut = nut
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 require("dotenv").config()
 const text = require("./const")
 
-const bot = new Telegraf(process.env.bot_token)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply(text.commands))
+const fs = require('fs');
 
-//bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+const bot = new Telegraf(process.env.bot_token)
+bot.start((ctx) => ctx.reply("Hi"))
+bot.help((ctx) => ctx.reply(text.commands))
+bot.command("positions", (ctx)=>{
+    ctx.replyWithHTML('<b>Виберіть позицію про яку хочете дізнатися інформацію</b>', Markup.inlineKeyboard(
+        [
+            [Markup.button.callback("Nut","btn_nut")]
+        ]
+    ))
+})
+
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply("Hi, bro"))
+
+bot.action('btn_nut', async (ctx)=>{
+    await ctx.answerCbQuery()
+    try {
+        ctx.reply(text.nut)
+    } catch (e){
+        console.loge(e)
+    }
+    
+})
 
 bot.launch()
 
