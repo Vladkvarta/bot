beba13882c4915412e47d17aa6010dacaa5ac8e2
Vladkvarta <Vladkvarta@gmail.com>
2025-07-12 22:31:00 +0300

    Add PM2 deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // Настройки вашего приложения
+  apps: [{
+    name: 'app.js',
+    script: 'index.js', // Убедитесь, что имя файла верное (у вас это index.js)
+  }],
+
+  // Настройки развертывания
+  deploy: {
+    production: {
+      user: 'admin',
+      host: 'ssh.smakuie.com', // Адрес для SSH, который мы настроили
+      ref: 'origin/main', // Ветка в Git
+      repo: 'git@github.com:Vladkvarta/bot.git', // ВАЖНО: SSH-URL вашего репозитория
+      path: '~/telegram-app', // Папка на Orange Pi
+
+      // Команды после загрузки нового кода
+      'post-deploy': 'npm install && /home/admin/.nvm/versions/node/v22.17.0/bin/pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
